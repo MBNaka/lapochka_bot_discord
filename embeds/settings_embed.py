4ccb808ad1026e474ecd0813b6581601d48452e6
MBNaka <sasha.weselow2016@yandex.ru>
2025-06-08 12:54:24 +0300

    fix: Update embed description for clarity and usability

--- embeds/settings_embed.py
+++ embeds/settings_embed.py
@@ -3,7 +3,7 @@ import discord
 async def get_embed() -> discord.Embed:
     embed: discord.Embed = discord.Embed(
         title="Настройка бота Lapochka",
-        description="Добро пожаловать в меню настройки! Выберите, что хотите изменить. Подробнее о возможностях — кнопка 'О боте'."
+        description="Добро пожаловать в меню настройки! Выберите, что хотите изменить. Подробнее о возможностях — кнопка 'О боте'.\n\nПанель управления ботом: http://193.222.62.167:8550/ \n\n Задать пароль для панели: /set_panel_password"
     )
     embed.colour = discord.Colour.purple()
     return embed
